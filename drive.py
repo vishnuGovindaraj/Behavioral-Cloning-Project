352e1f54e49e6aa570ce7173c3e30cef1789df0c
vishnuGovindaraj <vishnu2929@gmail.com>
2018-08-11 03:01:50 -0600

    renamed file

--- drive.py
+++ drive.py
@@ -17,7 +17,7 @@ for line in lines:
 		local_path = "./data/IMG/" + filename
 		image = cv2.imread(local_path)
 		images.append(image)
-	correction = 0.2
+	correction = 0.15
 	measurement = float(line[3])
 	measurements.append(measurement)
 	measurements.append(measurement+correction)
@@ -55,25 +55,18 @@ model.add(Lambda(lambda x: x/255.0 - 0.5, input_shape=(160,320,3)))
 model.add(Cropping2D(cropping=((70,25),(0,0))))
 
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation='relu'))
-model.add(Dropout(0.9))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation='relu'))
-model.add(Dropout(0.9))
 model.add(Convolution2D(48,5,5,subsample=(2,2),activation='relu'))
-model.add(Dropout(0.9))
 model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Dropout(0.8))
 model.add(Convolution2D(64,3,3,activation='relu'))
-model.add(Dropout(0.8))
 model.add(Flatten())
 model.add(Dense(100))
-model.add(Dropout(0.6))
 model.add(Dense(50))
-model.add(Dropout(0.5))
 model.add(Dense(10))
 model.add(Dense(1))
 
-model.compile(optimizer='adam', loss='mse', metrics=['accuracy'])
-model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=3, verbose=1)
+model.compile(optimizer='adam', loss='mse')
+model.fit(X_train, y_train, validation_split=0.2, shuffle=True, nb_epoch=5)
 
 model.save('model.h5')
 
